1a392f477a932ef80494a3497c7761d6d5db549c
DeekshaAr <deekshaarora418@gmail.com>
2024-06-12 08:38:52 +0530

    Proxyurl

--- src/App.js
+++ src/App.js
@@ -3,45 +3,44 @@ import axios from "axios";
 import "./App.css";
 import NavInshort from "./components/NavInshort";
 import NewsContent from "./components/NewsContent/NewsContent";
-import apikey from "./data/config";
 import Footer from "./components/Footer";
 
 function App() {
   const [category, setCategory] = useState("general");
   const [newsArray, setNewsArray] = useState([]);
   const [newsResults, setNewsResults] = useState([]);
-  const [loadmore, setLoadmore] = useState(20); //display 20 news
+  const [loadmore, setLoadmore] = useState(20); // display 20 news
 
   const newsApi = async () => {
     try {
+      const apiKey = process.env.REACT_APP_API_KEY;
       const news = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${loadmore}`
+        `/api/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`
       );
       setNewsArray(news.data.articles);
       setNewsResults(news.data.totalResults);
-      //console.log(news.data.articles);
     } catch (error) {
       console.log("Error fetching news:", error);
     }
   };
 
-  console.log(newsArray);
-
-  //Used when newsresults or category is going to change
+  // Used when newsResults or category is going to change
   useEffect(() => {
     newsApi();
-    //eslint-disable-next-line
-  }, [newsResults, category, loadmore]);
+    // eslint-disable-next-line
+  }, [category, loadmore]);
 
   return (
     <div className="App">
       <NavInshort setCategory={setCategory} />
-      <NewsContent
-        setLoadmore={setLoadmore}
-        loadmore={loadmore}
-        newsArray={newsArray}
-        newsResults={newsResults}
-      />
+      {newsResults && (
+        <NewsContent
+          setLoadmore={setLoadmore}
+          loadmore={loadmore}
+          newsArray={newsArray}
+          newsResults={newsResults}
+        />
+      )}
       <Footer />
     </div>
   );
--- src/data/config.js
+++ src/data/config.js
@@ -1,3 +1,3 @@
-const apiKey = "e72d44fdf2b9429fa16904beda752841";
+const REACT_APP_API_KEY = "e72d44fdf2b9429fa16904beda752841";
 
-export default apiKey;
+export default REACT_APP_API_KEY;
